9615a11dfc742d5cca19e6bd0824d5642d1b05e3
Bishoy <bishoyhany31@hotmail.com>
2020-08-22 18:09:12 +0200

    Final project merged together

--- app/src/main/java/com/example/leado/couse1Activities/VideoActivity.kt
+++ app/src/main/java/com/example/leado/couse1Activities/VideoActivity.kt
@@ -1,8 +1,17 @@
 package com.example.leado.couse1Activities
 
 import android.os.Bundle
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import com.example.leado.R
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import kotlinx.android.synthetic.main.fragment_lesson1.*
+import kotlinx.android.synthetic.main.fragment_lesson2.*
+import kotlinx.android.synthetic.main.fragment_lesson3.*
+import kotlinx.android.synthetic.main.fragment_lesson4.*
+import kotlinx.android.synthetic.main.fragment_refrence.*
 
 
 class VideoActivity : AppCompatActivity() {
@@ -10,7 +19,19 @@ class VideoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
 
-
+        initVideo(youtube_videoPlayer)
+        initVideo(youtube_videoPlayer2)
+        initVideo(youtube_videoPlayer3)
+        initVideo(youtube_videoPlayer4)
+        initVideo(youtube_videoPlayer5)
     }
 
+    fun initVideo(youtube_view: YouTubePlayerView) {
+        val videoid = "n0gNe0xVKo8"
+        youtube_view.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(@NonNull youTubePlayer: YouTubePlayer) {
+                youTubePlayer.loadVideo(videoid, 0f)
+            }
+        })
+    }
 }
--- app/src/main/java/com/example/leado/couse3Activities/VideoActivity3.kt
+++ app/src/main/java/com/example/leado/couse3Activities/VideoActivity3.kt
@@ -2,11 +2,34 @@ package com.example.leado.couse3Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.NonNull
 import com.example.leado.R
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import kotlinx.android.synthetic.main.fragment_lesson1_3.*
+import kotlinx.android.synthetic.main.fragment_lesson2_3.*
+import kotlinx.android.synthetic.main.fragment_lesson3_3.*
+import kotlinx.android.synthetic.main.fragment_lesson4_3.*
+import kotlinx.android.synthetic.main.fragment_refrence_3.*
 
 class VideoActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_3)
+        initVideo(youtube_videoPlayer)
+        initVideo(youtube_videoPlayer2)
+        initVideo(youtube_videoPlayer3)
+        initVideo(youtube_videoPlayer4)
+        initVideo(youtube_videoPlayer5)
+    }
+
+    fun initVideo(youtube_view: YouTubePlayerView) {
+        val videoid = "n0gNe0xVKo8"
+        youtube_view.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(@NonNull youTubePlayer: YouTubePlayer) {
+                youTubePlayer.loadVideo(videoid, 0f)
+            }
+        })
     }
 }
